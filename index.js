1c37d6d7987f0b5c45745c8d6e7f6ce494bf4aee
shakia-ripa <shakiaripa@gmail.com>
2023-10-17 02:34:43 +0600

    created api to post data on db

--- index.js
+++ index.js
@@ -25,12 +25,28 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const usersCollection = client.db('usersCollectionDB').collection('users');
+
+        app.get('/users', async (req, res) =>{
+            const cursor = usersCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.post('/users', async (req, res) =>{
+            const newUser = req.body;
+            console.log(newUser);
+            const result = await usersCollection.insertOne(newUser);
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
